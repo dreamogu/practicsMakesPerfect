2bad45af7468e7d52e8b38dd3c3ae8d09395a731
dreamogu <dreamogu59@gmail.com>
2023-06-10 18:05:37 +0900

    header search 완료

--- youtube/src/components/Header.tsx
+++ youtube/src/components/Header.tsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { GoSearch } from 'react-icons/go';
+import { useNavigate, useParams } from 'react-router';
 
 export default function Header() {
-  return <div>헤더</div>;
+  const { keyword } = useParams();
+  const navigate = useNavigate();
+  const [text, setText] = useState('');
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    navigate(`/videos/${text}`);
+    setText('');
+  };
+  useEffect(() => setText(keyword || ''), [keyword]);
+  // useEffect로 인해 뒤로가기나
+  return (
+    <header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button>
+          <GoSearch />
+        </button>
+      </form>
+    </header>
+  );
 }
